352d704dc54ccd8b3d0ab0aff55127ab0e7af54d
Becky G <Beckygarding@gmail.com>
2023-02-21 14:57:09 -0700

    react init

---
+++ static/index.js
@@ -0,0 +1,19 @@
+
+
+const Nav = (
+    <nav>
+        <h1>Threads 4 Pups</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+
+
+ReactDOM.render(
+Nav, 
+
+document.querySelector("#root"))
