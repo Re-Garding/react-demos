23e5c290ebc195253319987ea542d4fd9aaab9d0
Becky G <Beckygarding@gmail.com>
2023-03-19 09:26:59 -0600

    initiate vite

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import MainContent from "./Content"
+import Header from "./Header"
+import Footer from "./Footer"
+
+export default function App() {
+  return (
+      <div id='reasons'>
+          <Header />
+          <MainContent />
+          <Footer />
+      </div>
+  )
+}
